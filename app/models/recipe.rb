32bd09d12ddf5755164cb2084bb4375d57a24a2a
kchanin <kchanin@gmail.com>
2016-02-01 17:49:02 +0000

    add image uploader

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,14 @@ class Recipe < ActiveRecord::Base
   validates :name, presence: true, length: {minimum: 5, maximum: 100}
   validates :summary, presence: true, length: 10..150
   validates :description, presence: true, length: 20..500
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+  
+  private
+    def picture_size
+      if picture.size > 5.megabytes
+        error.add(:picure, "should be less than 5MB")
+      end
+    end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+require 'carrierwave/orm/activerecord' #this fixes the unitialized constant error in the console
