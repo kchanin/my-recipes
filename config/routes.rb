cb97b7d5ede09777b9e428299534a2bd059ff6c7
kchanin <kchanin@gmail.com>
2016-02-05 01:58:18 +0000

    Add ingredient/style navigation

--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,9 @@ Rails.application.routes.draw do
  post '/login', to: "logins#create"
  get '/logout', to: "logins#destroy"
  
+
+
+ resources :ingredients, only: [:new, :create, :show]
+ resources :styles, only: [:new, :create, :show]
+ 
 end
